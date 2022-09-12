552ede8f11e55d3c1047f0034f891d808e2abb56
Abidur Niloy <niloyabidur@gmail.com>
2022-09-13 04:21:58 +0600

    get method add

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 const express = require('express')
 const app = express()
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zbtoj.mongodb.net/?retryWrites=true&w=majority`;
 console.log('Database connected', uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-//   client.close();
-});
+async function run() {
+  try {
+    await client.connect();
+    const orderCollection = client.db('one-step-solution').collection('order')
+    app.get('/order', async(res, req)=>{
+      const query = {}
+      const cursor = orderCollection.find(query)
+      const orders = await cursor.toArray();
+      res.send(orders)
+    })
+    app.get('/order/:id',async(req, res)=>{
+      const id = req.params.id
+      const query = {_id: ObjectId(id)}
+      const result = await orderCollection.findOne(query)
+      res.send(result)
+    })
+    
+  } 
+  finally{
+    
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
