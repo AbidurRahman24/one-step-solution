3e71789aa4479e0c7ece05df2292c941260aa580
Abidur Niloy <niloyabidur@gmail.com>
2022-12-05 15:36:01 +0600

    deploy

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const orderCollection = client.db('one-step-solution').collection('order')
     const productCollection = client.db('one-step-solution').collection('product')
 
